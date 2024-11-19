15f9bb849b1632307fc540ced3ad197a240d25fa
amarildodossantos00 <amarildodossantosoficial@gmail.com>
2024-11-19 02:09:40 -0600

    atualizando o conceito de string

---
+++ study/session-3/print.py
@@ -0,0 +1,5 @@
+print(12 + 8);
+
+print(2, 0, sep="");
+print(2, 0, end=" ");
+print(2, 0);
---
+++ study/session-3/string.py
@@ -0,0 +1,13 @@
+print(1234);
+
+# Aspas simples
+print('Amarildo');
+
+# Aspas duplas
+print("COD ALIEN");
+
+# Escape
+print("\"amdos-sa\"")
+
+# r
+print(r"\"amdos-sa\"")

ce87dc9df05730754b5e7c0ff4e50cbbe7ff9e5e
amarildodossantos00 <amarildodossantosoficial@gmail.com>
2024-11-20 14:32:53 +0100

    conversao de tipos

---
+++ study/session-3/aula6.py
@@ -0,0 +1,9 @@
+# Conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# tipos imutáveis e primitivos:
+# str, int, float, bool
+
+print(int('10') - 3)
+print(float('10') - 3)
+print(bool('7'))
+print(str(7) + 'a')

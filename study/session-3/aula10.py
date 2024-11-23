436fde1d440da892406827d6f7b6b70358b4ecb9
amarildodossantos00 <amarildodossantosoficial@gmail.com>
2024-11-23 16:31:04 +0100

    concatencao e muito mais

---
+++ study/session-3/aula10.py
@@ -0,0 +1,8 @@
+concatenacao = "Amarildo" + " dos " + "Santos"
+print(concatenacao)
+
+a_sete_vezes = "A" * 7
+print(a_sete_vezes)
+
+sete_vezes_alien = ("Alien" + "\n") * 7
+print(sete_vezes_alien)

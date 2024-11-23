4fe7ec5c1b68b4269d4b546bbc97137d633d10ad
amarildodossantos00 <amarildodossantosoficial@gmail.com>
2024-11-23 16:19:31 +0100

    operadores aritimeticos

---
+++ study/session-3/aula9.py
@@ -0,0 +1,20 @@
+adicao = 3 + 4
+print("Adicao", adicao)
+
+subt = 10 - 3
+print("Subtracao", subt)
+
+mult = int(3.5 * 2)
+print("Multiplicacao", mult)
+
+divisao = int(14 / 2)
+print("Divisao", divisao)
+
+divisao_inteira = 14 // 2
+print("Divisao inteira", divisao_inteira)
+
+expon = 2 ** 2 + 3
+print("Exponenciacao", expon)
+
+modulo = 17 % 10
+print("Modulo", modulo)

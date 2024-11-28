ea3cf3f234dcb1c9bae0ec0a3dd28e9616e408b7
amarildodossantos00 <amarildodossantosoficial@gmail.com>
2024-11-28 08:43:29 +0100

    if else elif

---
+++ study/session-3/aula17.py
@@ -0,0 +1,12 @@
+condicao = False
+
+if condicao:
+	print("Este e o codigo do if")
+else:
+	print('Este e o codigo do else 1')
+
+if 7 == 7:
+	print("Outro if")
+else:
+	print("Este e o codigo do else 2")
+print("Fora do if")
